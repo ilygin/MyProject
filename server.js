28b0be04ae81fad7168e45040c697105d20683a6
Ed-Maron <lazycoast102@gmail.com>
2018-11-25 17:57:48 +0400

    Connection

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const connectionDb = require('./config/config').development;
 const PORT = 3000;
-const knexConnection = require('knex')(require('./config/config').development);
+const knex = require('knex')({
+    client: 'mysql',
+    connection: connectionDb
+});
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-require('./routes')(app, knexConnection);
+require('./routes')(app, knex);
 
 app.listen(PORT, () => {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
