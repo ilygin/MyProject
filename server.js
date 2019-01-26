5159459ae564ea61051159d4f971647eb70091e6
Ed-Maron <lazycoast102@gmail.com>
2019-01-26 21:20:36 +0400

    Переход на bcryptjs

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/* for dev mode    
+/* for dev mode  
 app.use(webpackDevMiddleware(compiler));
 /**/
 app.use(express.static(path.join(__dirname, 'dist')));
