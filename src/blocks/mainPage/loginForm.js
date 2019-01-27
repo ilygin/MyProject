c3192e7612c00be97294af08ea5a04b17b0fc81b
Igor Lygin <lazycoast102@gmail.com>
2019-01-28 02:15:31 +0400

    Главная страница.

--- src/blocks/mainPage/loginForm.js
+++ src/blocks/mainPage/loginForm.js
@@ -47,24 +47,22 @@ class LoginForm extends React.Component {
             return <Redirect to={"/account"} />
         }else if (!this.props.loginUser.isFetching && !this.props.loginUser.isAuth && this.props.loginUser.isAuth !== null) {
             return (
-
-                    <form method="post" className="registrationForm">
-                        <fieldset>
-                            <div className="form-group">
-                                <input type="email" className="form-control mb-2 emailInput" aria-describedby="emailHelp"
-                                       placeholder="Почта"/>
-                                <input type="password" className="form-control mt-2 passwordInput"
-                                       placeholder="Пароль"/>
-                            </div>
-                            {this.props.newUser.status === "error" ? <div><h3>{this.props.newUser.msg}</h3></div> :
-                                (this.props.loginUser.statusLogin === "error" || this.props.loginUser.statusLogin === "failure")?
-                                    <div><h3>{this.props.loginUser.msgLogin}</h3></div> :
-                                        this.props.newUser.status === "success" ? <div><h3>Поздравляю с успешной регистрацией</h3></div> : (false)}
-                            <button onClick={this.onLogInUserClick} type="submit" className="btn btn-primary">Войти</button>
-                            <button onClick={this.onSignUpClick} type="button" className="btn btn-outline-primary ml-1">Я новичок</button>
-                        </fieldset>
-                    </form>
-                
+                <div className="right-container__login-form">
+                        <div className="login-form__inputs-group">
+                            <input type="email" className="inputs-group__item" aria-describedby="emailHelp"
+                                   placeholder="Почта"/>
+                            <input type="password" className="inputs-group__item"
+                                   placeholder="Пароль"/>
+                        </div>
+                        {this.props.newUser.status === "error" ? <div><h3>{this.props.newUser.msg}</h3></div> :
+                            (this.props.loginUser.statusLogin === "error" || this.props.loginUser.statusLogin === "failure")?
+                                <div><h3>{this.props.loginUser.msgLogin}</h3></div> :
+                                    this.props.newUser.status === "success" ? <div><h3>Поздравляю с успешной регистрацией</h3></div> : (false)}
+                        <div className="login-form__button-groups">
+                            <button className="button-groups__login" onClick={this.onLogInUserClick} type="submit">Войти</button>
+                            <button className="button-groups__signup" onClick={this.onSignUpClick} type="button">Я новичок</button>
+                        </div>
+                </div>
             )
         }else {
              return <div />
